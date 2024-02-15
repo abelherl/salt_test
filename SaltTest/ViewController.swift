3e3a80a66f3624094bcce0e93e881dc767352360
abelherl <abangabel02@gmail.com>
2024-02-15 07:55:12 +0700

    Initial Commit

---
+++ SaltTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SaltTest
+//
+//  Created by Abel Herlambang on 15/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
